f9fc3c5b806f7a71ad88d48ae795407f43a14076
frankos6 <frankos6@wp.pl>
2023-02-17 13:57:33 +0100

    Created SCP914RoughKill plugin

---
+++ SCP914RoughKill/Config.cs
@@ -0,0 +1,10 @@
+﻿using Exiled.API.Interfaces;
+
+namespace SCPPlugins.SCP914RoughKill
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; }
+        public bool Debug { get; set; }
+    }
+}
---
+++ SCP914RoughKill/SCP914RoughKill.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Scp914;
+using Exiled.Events.Handlers;
+using Scp914;
+
+namespace SCPPlugins.SCP914RoughKill
+{
+    // ReSharper disable once InconsistentNaming
+    public class SCP914RoughKill : Plugin<Config>
+    {
+        public override string Author => "frankos6";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(6, 0, 0, 0);
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Scp914.UpgradingPlayer += Scp914OnUpgradingPlayer;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Scp914.UpgradingPlayer -= Scp914OnUpgradingPlayer;
+            base.OnDisabled();
+        }
+        
+        private void Scp914OnUpgradingPlayer(UpgradingPlayerEventArgs ev)
+        {
+            if (ev.KnobSetting != Scp914KnobSetting.Rough) return;
+            ev.Player.Kill("Rozpierdolenie się totalne");
+        }
+    }
+}
