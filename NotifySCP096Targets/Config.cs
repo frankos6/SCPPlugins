1a57358b69261fa48a012f6d95dc87931940d564
frankos6 <frankos6@wp.pl>
2023-02-18 23:08:03 +0100

    Added NotifySCP096Targets plugin

---
+++ NotifySCP096Targets/Config.cs
@@ -0,0 +1,10 @@
+﻿using Exiled.API.Interfaces;
+
+namespace SCPPlugins.NotifySCP096Targets
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; }
+        public bool Debug { get; set; }
+    }
+}
---
+++ NotifySCP096Targets/NotifySCP096Targets.cs
@@ -0,0 +1,32 @@
+﻿using Exiled.API.Features;
+using System;
+using Exiled.Events.EventArgs.Scp096;
+
+namespace SCPPlugins.NotifySCP096Targets
+{
+    // ReSharper disable once InconsistentNaming
+    public class NotifySCP096Targets : Plugin<Config>
+    {
+        public override string Author => "frankos6";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(6, 0, 0, 0);
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Scp096.AddingTarget += Scp096OnAddingTarget;
+            base.OnEnabled();
+        }
+        
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Scp096.AddingTarget -= Scp096OnAddingTarget;
+            base.OnDisabled();
+        }
+        
+        /// <inheritdoc cref="Exiled.Events.Handlers.Scp096.OnAddingTarget"/>
+        private static void Scp096OnAddingTarget(AddingTargetEventArgs ev)
+        {
+            ev.Target.ShowHint("You are targeted by SCP-096",5f);
+        }
+    }
+}
