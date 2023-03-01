7a67c5bd268d9d39c1dd2ae95049b3a6ea37a676
frankos6 <frankos6@wp.pl>
2023-03-01 18:35:15 +0100

    Added RespawnWaveInfo plugin

---
+++ RespawnWaveInfo/Config.cs
@@ -0,0 +1,10 @@
+﻿using Exiled.API.Interfaces;
+
+namespace SCPPlugins.RespawnWaveInfo
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; }
+        public bool Debug { get; set; }
+    }
+}
